cd869513058b23b714871d2ac972cb87ae6ef9d5
kotIIT <mkotlyar@hawk.iit.edu>
2020-03-07 18:57:37 -0600

    Webserver can execute code via javascript

--- webInterface/Readme.md
+++
@@ -1,7 +0,0 @@
-## Interface Setup
-
-    cd webInterface
-    pipenv install #(only do once)
-    pipenv shell
-    export FLASK_APP=webInterface.py
-    flask run
--- webInterface/app/routes.py
+++ webInterface/app/routes.py
@@ -8,19 +8,26 @@ FILE_DIR_PATH = os.path.dirname(__file__)
 SCAN_DATA_PATH = os.path.join(FILE_DIR_PATH, '../../scannedlist.xml')
 
 
+# background process happening without any refreshing
+@app.route('/background_process_test')
+def background_process_test():
+    print("someone wants the background test")
+    return 'Hi from the web API :)'
+
+
 @app.route('/')
 @app.route('/index')
 def index():
-	scanresults = None
+    scanresults = None
 
-	if os.path.exists(SCAN_DATA_PATH):
-		scanresults = ""
-		with open(SCAN_DATA_PATH, 'r') as f:
-			for line in f:
-				scanresults += line
-	else:
-		print("scannedlist.xml does not exist.")
-		print(os.path.abspath(SCAN_DATA_PATH))
-		scanresults = None
+    if os.path.exists(SCAN_DATA_PATH):
+        scanresults = ""
+        with open(SCAN_DATA_PATH, 'r') as f:
+            for line in f:
+                scanresults += line
+    else:
+        print("scannedlist.xml does not exist.")
+        print(os.path.abspath(SCAN_DATA_PATH))
+        scanresults = None
 
-	return render_template('index.html', scanresults=scanresults)
+    return render_template('index.html', scanresults=scanresults)
