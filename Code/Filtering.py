5c8a6f3d92cc11b5dcd03a461bb15d74b09a4c80
Darthseid <jared.mcvey@yahoo.com>
2020-03-09 23:12:57 -0500

    Added proper XML formatting.

--- Code/Filtering.py
+++ Code/Filtering.py
@@ -5,8 +5,10 @@ with open('class-scan.xml', 'r') as myfile: #This turns the entire contents of t
   data = myfile.read()
 Substr = re.findall(r' addr="(.+?)" ',data) #addr is the beginning filter,, 
 Substr = [' {0} '.format(elem) for elem in Substr] #Adds spaces to the elements
-file = open('Filter_Output.xml','w') #This will have to be changed to overwrite the file instead.
+file = open('Filter_Output.xml','w')
 output = ''.join(Substr)
-file.write(output) 
+partone = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+parttwo = "<text>"
+partthree = "</text>"
+file.write(partone + parttwo + output + partthree) 
 file.close() 
-
