ae8535db8fbb499a9a78f0ae3f04003c5153839d
Darthseid <jared.mcvey@yahoo.com>
2020-03-09 22:45:01 -0500

    Gets all addresses and spaces them out.

--- Code/Filtering.py
+++ Code/Filtering.py
@@ -1,10 +1,9 @@
 import regex
 import re
 output = ""
-with open('Samplescan.xml', 'r') as myfile: #This turns the entire contents of the file into a string.
+with open('class-scan.xml', 'r') as myfile: #This turns the entire contents of the file into a string.
   data = myfile.read()
-Substr = re.findall(r' addr="(.+?)" ',data) #addr is the beginning filter,
-Substr += re.findall(r' name="(.+?)" ',data) #name is the beginning filter, 
+Substr = re.findall(r' addr="(.+?)" ',data) #addr is the beginning filter,, 
 Substr = [' {0} '.format(elem) for elem in Substr] #Adds spaces to the elements
 file = open('Filter_Output.xml','w') #This will have to be changed to overwrite the file instead.
 output = ''.join(Substr)
