97cb540538f985f773cf32970036713aba8ea499
Darthseid <jared.mcvey@yahoo.com>
2020-03-24 18:00:53 -0500

    Added quotes to Filtering

--- Code/Filtering.py
+++ Code/Filtering.py
@@ -4,7 +4,7 @@ output = ""
 with open('class-scan.xml', 'r') as myfile: #This turns the entire contents of the file into a string.
   data = myfile.read()
 Substr = re.findall(r' addr="(.+?)" ',data) #addr is the beginning filter,, 
-Substr = ['INSERT INTO XMLAddresses(Address) VALUES ({0}); \n '.format(elem) for elem in Substr] #Adds spaces to the elements
+Substr = ['INSERT INTO XMLAddresses(Address) VALUES ("{0}"); \n '.format(elem) for elem in Substr] #Adds spaces to the elements
 file = open('SQL_Output.sql','w') #Prints SQL code
 output = ''.join(Substr) #An SQL database with this table needs to be created first.
 file.write(output) 
