622e9b1798dc0a656544fd3a0a57ce647bfaba8b
jorell <jsocorro@hawk.iit.edu>
2020-04-29 13:23:21 -0500

    made sure to keep track which user is signed in

--- Code/databaseConn/Apache24/htdocs/startOverPhp/pages/includes/comment.inc.php
+++ Code/databaseConn/Apache24/htdocs/startOverPhp/pages/includes/comment.inc.php
@@ -1,6 +1,16 @@
 <?php
+    session_start();
     if(isset($_POST['comment'])) {
-        echo "comment page";
+        echo "comment page " . $_SESSION['username'];
+
+        require 'dbh.inc.php';
+
+        $comment = $_POST['inpComment'];
+
+        //variables for database
+        $sql = "SELECT uidUsers from usercom WHERE uidUsers=?";
+        $stmt = mysqli_stmt_init($conn);    
+
     }
     else {
         //error occurs
