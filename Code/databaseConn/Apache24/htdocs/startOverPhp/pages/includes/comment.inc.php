f9e1661da560bf934cff20577a6d30dc6cb9cea0
jorell <jsocorro@hawk.iit.edu>
2020-04-29 13:46:11 -0500

    User is able to push comment to database

--- Code/databaseConn/Apache24/htdocs/startOverPhp/pages/includes/comment.inc.php
+++ Code/databaseConn/Apache24/htdocs/startOverPhp/pages/includes/comment.inc.php
@@ -1,15 +1,23 @@
 <?php
     session_start();
     if(isset($_POST['comment'])) {
-        echo "comment page " . $_SESSION['username'];
+        //echo "comment page " . $_SESSION['username'];
 
         require 'dbh.inc.php';
 
+        $user = $_SESSION['username'];
         $comment = $_POST['inpComment'];
 
         //variables for database
-        $sql = "SELECT uidUsers from usercom WHERE uidUsers=?";
-        $stmt = mysqli_stmt_init($conn);    
+        $sql = "UPDATE usercom SET comments = '$comment' WHERE uidUsers = '$user'" ;
+        //$stmt = mysqli_stmt_init($conn);    
+
+        if (mysqli_query($conn,$sql)) {
+            echo "Record updated successfully";
+        }
+        else {
+            echo "failed";
+        }
 
     }
     else {
