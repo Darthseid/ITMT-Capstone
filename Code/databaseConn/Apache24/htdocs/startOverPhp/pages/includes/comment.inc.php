66bd289dfd5f4ab695f2cdfcc39d6bc8eaecd7a8
jorell <jsocorro@hawk.iit.edu>
2020-04-30 16:09:38 -0500

    added script for comment4 and comment5

--- Code/databaseConn/Apache24/htdocs/startOverPhp/pages/includes/comment.inc.php
+++ Code/databaseConn/Apache24/htdocs/startOverPhp/pages/includes/comment.inc.php
@@ -1,10 +1,11 @@
 <?php
     session_start();
+    
     if(isset($_POST['comment1'])) {
         //echo "comment page " . $_SESSION['username'];
 
         require 'dbh.inc.php';
-
+        echo '<script>alert("Hello"); </script>';
         $user = $_SESSION['username'];
         $comment = $_POST['inpComment'];
 
@@ -46,6 +47,42 @@
 
         //variables for database
         $sql = "UPDATE usercom SET comment3 = '$comment' WHERE uidUsers = '$user'" ;
+        ///$stmt = mysqli_stmt_init($conn);    
+
+        if (mysqli_query($conn,$sql)) {
+            header("Location: ../blogList.php?commentsuccess");
+            exit();
+        }
+        
+        mysqli_stmt_close($stmt);
+        mysqli_close($conn);
+    }
+    else if (isset($_POST['comment4'])){
+        require 'dbh.inc.php';
+
+        $user = $_SESSION['username'];
+        $comment = $_POST['inpComment'];
+
+        //variables for database
+        $sql = "UPDATE usercom SET comment4 = '$comment' WHERE uidUsers = '$user'" ;
+        ///$stmt = mysqli_stmt_init($conn);    
+
+        if (mysqli_query($conn,$sql)) {
+            header("Location: ../blogList.php?commentsuccess");
+            exit();
+        }
+        
+        mysqli_stmt_close($stmt);
+        mysqli_close($conn);
+    }
+    else if (isset($_POST['comment5'])){
+        require 'dbh.inc.php';
+
+        $user = $_SESSION['username'];
+        $comment = $_POST['inpComment'];
+
+        //variables for database
+        $sql = "UPDATE usercom SET comment5 = '$comment' WHERE uidUsers = '$user'" ;
         ///$stmt = mysqli_stmt_init($conn);    
 
         if (mysqli_query($conn,$sql)) {
