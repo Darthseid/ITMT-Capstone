9d685d864fdd6fd9638ed8a77297709560a1f495
jorell <jsocorro@hawk.iit.edu>
2020-04-30 03:53:01 -0500

    have a comment3 for comment backend interaction

--- Code/databaseConn/Apache24/htdocs/startOverPhp/pages/includes/comment.inc.php
+++ Code/databaseConn/Apache24/htdocs/startOverPhp/pages/includes/comment.inc.php
@@ -38,6 +38,24 @@
         mysqli_stmt_close($stmt);
         mysqli_close($conn);
     }
+    else if (isset($_POST['comment3'])){
+        require 'dbh.inc.php';
+
+        $user = $_SESSION['username'];
+        $comment = $_POST['inpComment'];
+
+        //variables for database
+        $sql = "UPDATE usercom SET comment3 = '$comment' WHERE uidUsers = '$user'" ;
+        ///$stmt = mysqli_stmt_init($conn);    
+
+        if (mysqli_query($conn,$sql)) {
+            header("Location: ../blogList.php?commentsuccess");
+            exit();
+        }
+        
+        mysqli_stmt_close($stmt);
+        mysqli_close($conn);
+    }
     else {
         //error occurs
         echo "error";
